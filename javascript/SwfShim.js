1fc811de0df8c3e5f4c1767e9c4886c8d022c77b
touvan <touvan@a4fad56a-dc34-0410-8684-cb6ad95a1988>
2009-05-21 21:34:31 +0000

    - Major rewrite. - Fixes IE support. - Adds shimMode property (it's off by default).
    git-svn-id: http://unfocus-history-keeper.googlecode.com/svn/trunk@27 a4fad56a-dc34-0410-8684-cb6ad95a1988

--- javascript/SwfShim.js
+++ javascript/SwfShim.js
@@ -1,6 +1,6 @@
 /*
-unFocus.SwfHTML, version 2.0 (beta 2) (2007/09/11)
-Copyright: 2005-2007, Kevin Newman (http://www.unfocus.com/Projects/)
+unFocus.SwfHTML, version 2.0 (beta 3) (2009/21/05)
+Copyright: 2005-2009, Kevin Newman (http://www.unfocus.com/Projects/)
 
 This file is part of unFocus.History Keeper.
 
@@ -22,80 +22,111 @@ References:
 	<http://code.google.com/p/swfobject/wiki/faq>
 */
 unFocus.SwfShim = function(shimSwfUrl) {
-	unFocus.SwfHTML.call(this);
 	/*if (!shimSwfUrl && !unFocus.SwfShim.shimSwfUrl)
 		throw new Error (
-			"MissingArgument", 
-			"You must pass the URL of the SwfShim.swf to the constructor, or set the static property - unFocus.SwfShim.shimSwfUrl."
+			"Argument Error. You must pass the URL of the SwfShim.swf to the constructor, or set the static property - unFocus.SwfShim.shimSwfUrl."
 		);*/
+	unFocus.SwfHTML.call(this);
 	this._shimSwfUrl = shimSwfUrl || unFocus.SwfShim.shimSwfUrl;
 };
 // static properties and methods
-//SwfShim.shimSwfUrl = "";
 with (unFocus) {
 	// :NOTE: this is relative to the document, not this js source file
 	SwfShim.shimSwfUrl = "SwfShim.swf";
+	SwfShim.prototype = new SwfHTML;
 }
-// new methods and overrides
-unFocus.SwfShim.prototype = {
-	__proto__: unFocus.SwfHTML.prototype,
-	_realSrc: "",
-	_MMdocumentTitle: escape(document.title),
-	_MMredirectURL: escape(window.location),
+with (unFocus.SwfShim) {
+	// new methods and overrides
+	prototype._realSrc = "";
+	prototype._MMdocumentTitle = escape(document.title);
+	prototype._MMredirectURL = escape(window.location);
 	// :NOTE: This should be overridden by the user.
-	eiCallbackName: false,
-	expressInstall: false,
-	getHTML: function() {
-		var theHTML;
-		unFocus.SwfHTML.prototype.setSrc.call(this,this._shimSwfUrl);
-		this.addFlashvar("movieSrc", this._realSrc);
-		
-		if (this.expressInstall) {
-			this.addFlashvar("useExpressInstall", "true");
-			this.addFlashvar("reqVersion", this._version);
-			this.addFlashvar("reqMajorRevision", this._majorRevision);
-			this.addFlashvar("reqMinorRevision", this._minorRevision);
-			
-			var version = this._version,
-				minorRev = this._minorRevision;
-			
-			this.setVersion(6);
-			this.setMinorRevision(65);
-			
-			this.addFlashvar("MMdocumentTitle", this._MMdocumentTitle);
-			this.addFlashvar("MMredirectURL", this._MMredirectURL);
-			
-			if (this.eiCallbackName)
-				this.addFlashvar("eiCallbackName", this.eiCallbackName);
-			
-			if (/*@cc_on!@*/0)
-				this.addFlashvar("MMplayerType", "ActiveX");
-			else
-				this.addFlashvar("MMplayerType", "PlugIn");
-			
-			theHTML = unFocus.SwfHTML.prototype.getHTML.apply(this);
-			
-			this.setVersion(version);
-			this.setMinorRevision(minorRev);
-		}
-		else
-			theHTML = unFocus.SwfHTML.prototype.getHTML.apply(this);
-		// return unFocus.SwfHTML.getHTML.apply(this);
-		return unFocus.SwfHTML.prototype.getHTML.apply(this);
-	},
-	setSrc: function(src) {
+	prototype.eiCallbackName = false;
+	prototype.expressInstall = false;
+	prototype.shimMode = false;
+	
+	prototype.setSrc = function(src) {
 		this._realSrc = src;
-	},
-	setMMdocumentTitle: function(title) {
+	};
+	prototype.setMMdocumentTitle = function(title) {
 		this._MMdocumentTitle = title;
-	},
-	setMMredirectURL: function(url) {
+	};
+	prototype.setMMredirectURL = function(url) {
 		this._MMredirectURL = url;
-	},
-	setExpressInstallCallback: function(methodName) {
+	};
+	prototype.setExpressInstallCallback = function(methodName) {
 		this.eiCallbackName = methodName;
-	},
-	useExpressInstall: function(useEI) {
+	};
+	prototype.useExpressInstall = function(useEI) {
 		this.expressInstall = useEI;	
-	}
+	};
+	prototype.useShimMode = function(m) {
+		this.shimMode = m;	
+	};
 }
+unFocus.SwfShim.prototype.getHTML = function() {
+	var theHTML, useEI;
+	
+	if (this.shimMode)
+		this.addFlashvar("movieSrc", this._realSrc);
+	else
+		unFocus.SwfHTML.prototype.setSrc.call(this,this._realSrc);
+	
+	if (this.expressInstall) {
+		// :NOTE: Uses JS based detection. To use ExpressInstall, without shimMode
+		// you need to include unFocus.FlashPlayerInfo. Not needed otherwise.
+		with (unFocus.FlashPlayerInfo) {
+			var majorRevision = getMajorRevision();
+			var version = getVersion() + '.' + majorRevision;
+			var minorRevision = getMinorRevision();
+		}
+		
+		var reqVersion = this._version;
+		if (this._majorVersion)
+			reqVersion = reqVersion + '.' + this._majorVersion;
+		
+		var reqMinorRevision = this._minorRevision;
+		
+		if (version < reqVersion || (
+				reqMinorRevision && 
+				version == reqVersion && 
+				minorRevision < reqMinorRevision
+			)
+		) useEI = true;
+	}
+	
+	if (useEI) {
+		unFocus.SwfHTML.prototype.setSrc.call(this,this._shimSwfUrl);
+		
+		this.addFlashvar("useExpressInstall", "true");
+		this.addFlashvar("reqVersion", this._version);
+		this.addFlashvar("reqMajorRevision", this._majorRevision);
+		this.addFlashvar("reqMinorRevision", this._minorRevision);
+		
+		var version = this._version,
+			minorRev = this._minorRevision;
+		
+		this.setVersion(6);
+		this.setMinorRevision(65);
+		
+		this.addFlashvar("MMdocumentTitle", this._MMdocumentTitle);
+		this.addFlashvar("MMredirectURL", this._MMredirectURL);
+		
+		if (this.eiCallbackName)
+			this.addFlashvar("eiCallbackName", this.eiCallbackName);
+		
+		if (/*@cc_on!@*/0)
+			this.addFlashvar("MMplayerType", "ActiveX");
+		else
+			this.addFlashvar("MMplayerType", "PlugIn");
+		
+		theHTML = unFocus.SwfHTML.prototype.getHTML.apply(this);
+		
+		this.setVersion(version);
+		this.setMinorRevision(minorRev);
+	}
+	else
+		theHTML = unFocus.SwfHTML.prototype.getHTML.apply(this);
+	
+	return theHTML;
+};
